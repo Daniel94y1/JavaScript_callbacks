3e62ca1a6cc314fcd7a4ba7d3b923cf827a13031
Daniel94y1 <escobardaniel08@gmail.com>
2023-12-18 19:46:45 +0100

    Complete exercises

--- exercise_02/createUser.js
+++ exercise_02/createUser.js
@@ -1,3 +1,4 @@
+import validateString from "../helpers/validateString";
 /**
  * Generate a random number between 0 and 1.
  *
@@ -19,7 +20,7 @@ function generateRandomNumber() {
 function createUser(email, onError, onSuccess) {
   const randomNumber = generateRandomNumber();
 
-  if (randomNumber < 0.5 || email === "") {
+  if (randomNumber < 0.5 || !validateString(email)) {
     return onError();
   }
 
